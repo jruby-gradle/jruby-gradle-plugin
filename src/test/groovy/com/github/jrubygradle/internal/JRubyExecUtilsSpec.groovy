00e32860b052b976506d9b12b96bf51c09f35627
R. Tyler Croy <tyler@monkeypox.org>
2015-06-05 07:29:25 -0700

    Re-introduce support for closures in JRubyExec's scriptArgs
    This regressed in the refactoring of JRubyExecTraits since I didn't add good
    tests around the functionality

--- src/test/groovy/com/github/jrubygradle/internal/JRubyExecUtilsSpec.groovy
+++ src/test/groovy/com/github/jrubygradle/internal/JRubyExecUtilsSpec.groovy
@@ -3,30 +3,6 @@ package com.github.jrubygradle.internal
 import spock.lang.*
 
 class JRubyExecUtilsSpec extends Specification {
-    def "buildArgs should handle scriptArgs with closures"() {
-        given:
-        List<Object> scriptArgs = [{'spock'}]
-
-        when:
-        def cmdArgs = JRubyExecUtils.buildArgs(['-S'], new File('.'), scriptArgs)
-
-        then:
-        cmdArgs instanceof List<String>
-        cmdArgs.find { it == 'spock' }
-    }
-
-    def "buildArgs should handle scriptArgs with closures for non-JRuby commands"() {
-        given:
-        List<Object> scriptArgs = [{'spock'}]
-
-        when:
-        def cmdArgs = JRubyExecUtils.buildArgs([], new File('.'), scriptArgs)
-
-        then:
-        cmdArgs instanceof List<String>
-        cmdArgs.find { it == 'spock' }
-    }
-
     def "The version string in a jruby jar filename must be extracted correctly"() {
 
         expect:
