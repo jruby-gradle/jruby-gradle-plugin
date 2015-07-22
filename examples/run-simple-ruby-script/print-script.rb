1b16153ef6f4c4dc7c4fb3d8c374b6af2323c497
R. Tyler Croy <tyler@monkeypox.org>
2015-07-21 17:09:28 -0700

    Add a simple jruby-exec example for documentation purposes

---
+++ examples/run-simple-ruby-script/print-script.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+require 'colorize'
+
+puts "-" * 20
+puts "Ruby version: #{RUBY_VERSION}"
+puts "Ruby platform: #{RUBY_PLATFORM}"
+puts "-" * 20
+
+puts "Roses are red".red
+puts "Violets are blue".blue
+puts "I can use JRuby/Gradle".green
+puts "And now you can too!".yellow
