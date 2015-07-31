757e04011ef5f07cf1ae290853378d780b59b69a
R. Tyler Croy <tyler@monkeypox.org>
2015-07-31 07:04:23 -0700

    Remove a number of ignored/deprecated tests for JRubyJar

--- jruby-gradle-jar-plugin/src/test/groovy/com/github/jrubygradle/jar/JRubyJarPluginSpec.groovy
+++ jruby-gradle-jar-plugin/src/test/groovy/com/github/jrubygradle/jar/JRubyJarPluginSpec.groovy
@@ -75,23 +75,6 @@ class JRubyJarPluginSpec extends Specification {
         project.tasks.getByName('jrubyJar').appendix == 'jruby'
     }
 
-    @Ignore("gemDir doesn't make sense")
-    def "Adding a fake file as if it is a gem layout"() {
-        when: 'We configure the jar task with jruby data'
-        new File(TESTROOT,'fake.txt').text = 'fake.content'
-        project.configure(jarTask) {
-            jruby {
-                gemDir TESTROOT
-            }
-        }
-
-        Set<String> names = fileNames(jarTask.source)
-
-        then: 'Expecting jar task'
-        !jarTask.manifest.attributes.containsKey('Main-Class')
-        names == (['MANIFEST.MF','fake.txt'] as Set<String>)
-    }
-
     def "Fails on adding non-existing initScript"() {
         given:
         project.jruby.gemInstallDir = TESTROOT.absolutePath
@@ -106,24 +89,6 @@ class JRubyJarPluginSpec extends Specification {
 
         then: "Then expecting use error"
         thrown(InvalidUserDataException)
-    }
-
-    @Ignore("gemDir doesn't make sense")
-    def "Adding the default gem directory"() {
-        given:
-        project.jruby.gemInstallDir = TESTROOT.absolutePath
-        new File(TESTROOT,'gems').mkdirs()
-        new File(TESTROOT,'gems/fake.txt').text = 'fake.content'
-
-        when: "Setting a default main class"
-        project.configure(jarTask) {
-            jruby {
-                defaultGems()
-            }
-        }
-
-        then: "Then expecting that directory to be found"
-        fileNames(jarTask.source) == (['MANIFEST.MF','gems','gems/fake.txt'] as Set<String>)
     }
 
     def "Adding a default main class"() {
@@ -148,43 +113,6 @@ class JRubyJarPluginSpec extends Specification {
         jarTask.manifest.attributes.'Main-Class' == JRubyJar.EXTRACTING_MAIN_CLASS
     }
 
-    @Ignore("gemDir doesn't make sense")
-    def "Adding all defaults"() {
-        given: "Given some files in a gem location or which some should be excluded"
-        File gems = new File(TESTROOT,'gems')
-        gems.mkdirs()
-        project.jruby.gemInstallDir = gems
-        new File(gems,'gems').mkdirs()
-        new File(gems,'data').mkdirs()
-        new File(gems,'cache').mkdirs()
-        new File(gems,'gems/fake.txt').text = 'fake.content'
-        new File(gems,'data/data.txt').text = 'data.content'
-
-        File jars = new File(TESTROOT,'jars')
-        jars.mkdirs()
-        project.jruby.jarInstallDir = jars
-        new File(jars,'fake.jar').text = 'fake.content'
-        new File(jars,'Jars.lock').text = 'fake'
-
-        File init = new File(TESTROOT,'init.rb')
-        init.text = 'fake.content'
-
-        when: "Setting a default main class and default gems via the 'defaults' method"
-        project.configure(jarTask) {
-            defaults 'gems','mainClass'
-            initScript "${init.absolutePath}"
-        }
-        jarTask.applyConfig()
-
-        then: "The appropriate files included"
-        fileNames(jarTask.source).containsAll(['MANIFEST.MF', 'gems', 'gems/fake.txt',
-                                               'data', 'data/data.txt', 'fake.jar',
-                                               'Jars.lock', 'init.rb'])
-
-        and: "Then the attribute should be set to the default in the manifest"
-        jarTask.manifest.attributes.'Main-Class' == JRubyJar.DEFAULT_MAIN_CLASS
-    }
-
     def "Adding a custom main class"() {
         when: "Setting a default main class"
         project.configure(jarTask) {
@@ -196,7 +124,6 @@ class JRubyJarPluginSpec extends Specification {
         jarTask.manifest.attributes.'Main-Class' == 'org.scooby.doo.snackMain'
     }
 
-    @Ignore("needs rework")
     def "Setting up a java project"() {
         given: "All jar, java plugins have been applied"
         project = setupProject()
@@ -207,39 +134,25 @@ class JRubyJarPluginSpec extends Specification {
         File expectedDir= new File(TESTROOT,'libs/')
         expectedDir.mkdirs()
         project.configure(jar) {
-            destinationDir = expectedDir
             initScript library()
         }
         jar.applyConfig()
 
         expect:
-        jar.taskDependencies.getDependencies(jar).contains(project.tasks.getByName('jrubyPrepare'))
+        jar.taskDependencies.getDependencies(jar).contains(project.tasks.getByName('prepareJRubyJar'))
     }
 
-    @Ignore("needs rework after gemDir dies")
     def "Building a Jar with a custom configuration and 'java' plugin is applied"() {
         given: "Java plugin applied before JRuby Jar plugin"
         project = setupProject()
         project.apply plugin : 'java'
         Task jrubyJar = project.tasks.getByName('jrubyJar')
-
-        and: "A local repository"
-        File expectedDir= new File(TESTROOT,'libs/')
-        expectedDir.mkdirs()
-        File expectedJar= new File(expectedDir,'test-jruby.jar')
-        project.jruby.gemInstallDir = new File(TESTROOT,'fakeGemDir').absolutePath
-
-        new File(project.jruby.gemInstallDir,'gems').mkdirs()
-        new File(project.jruby.gemInstallDir,'gems/fake.txt').text = 'fake.content'
+        File expectedDir = new File(TESTROOT, 'libs/')
+        File expectedJar = new File(expectedDir, 'test-jruby.jar')
 
         when: "I set the main class"
         project.configure(jrubyJar) {
-            destinationDir = expectedDir
-            jruby {
-                defaultGems()
-                mainClass 'bogus.does.not.exist'
-                initScript runnable()
-            }
+            mainClass 'bogus.does.not.exist'
         }
         project.evaluate()
 
@@ -249,9 +162,6 @@ class JRubyJarPluginSpec extends Specification {
 
         then: "I expect to see jruby.home unpacked "
         builtJar.contains("META-INF/jruby.home/lib/ruby".toString())
-
-        and: "To see my fake files in the 'gems' folder"
-        builtJar.contains("gems/fake.txt".toString())
 
         and: "I expect the new main class to be listed in the manifest"
         jrubyJar.manifest.effectiveManifest.attributes['Main-Class']?.contains('bogus.does.not.exist')
