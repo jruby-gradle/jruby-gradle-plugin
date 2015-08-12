263e9a6c86078bacf87d6ad828ac06f4ed898e71
R. Tyler Croy <tyler@monkeypox.org>
2015-08-11 17:12:59 -0700

    Introduce a very simple integration test for the default jrubyJar task
    References #185

---
+++ jruby-gradle-jar-plugin/src/integTest/groovy/com/github/jrubygradle/jar/JRubyJarIntegrationSpec.groovy
@@ -0,0 +1,55 @@
+package com.github.jrubygradle.jar
+
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+import org.gradle.testkit.runner.BuildResult
+import org.gradle.testkit.runner.GradleRunner
+import org.gradle.testkit.runner.TaskOutcome
+
+import spock.lang.*
+
+class JRubyJarIntegrationSpec extends Specification {
+    @Rule
+    final TemporaryFolder testProjectDir = new TemporaryFolder()
+    File buildFile
+    String pluginDependencies
+
+    def setup() {
+        buildFile = testProjectDir.newFile('build.gradle')
+        def pluginClasspathResource = getClass().classLoader.findResource("plugin-classpath.json")
+
+        if (pluginClasspathResource == null) {
+            throw new IllegalStateException("Did not find plugin classpath resource, run `testClasses` build task.")
+        }
+
+        pluginDependencies = pluginClasspathResource.text
+    }
+
+    def "executing the jrubyJar default task produces a jar artifact"() {
+        given:
+        buildFile << """
+buildscript {
+    dependencies {
+        classpath files(${pluginDependencies})
+    }
+}
+apply plugin: 'com.github.jruby-gradle.jar'
+
+repositories { jcenter() }
+
+jrubyJar {
+}
+    """
+
+        when:
+        BuildResult result = GradleRunner.create()
+                .withProjectDir(testProjectDir.root)
+                .withArguments('jrubyJar')
+                .build()
+
+        then:
+        File[] artifacts = (new File(testProjectDir.root, ['build', 'libs'].join(File.separator))).listFiles()
+        artifacts && artifacts.size() == 1
+        result.task(":jrubyJar").outcome == TaskOutcome.SUCCESS
+    }
+}
