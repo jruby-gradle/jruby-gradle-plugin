824cb8ae63d2d2d5648864754df86ca885b3d15c
R. Tyler Croy <rtyler@brokenco.de>
2019-05-25 14:53:56 -0700

    Add documentation and an example for jrubyArgs on the JRubyExec task
    Turns out we had jrubyArgs all along, but never _really_ documented it with the
    other arguments.
    
    Fixes #359

---
+++ examples/run-script-with-jruby-args/print-script.rb
@@ -0,0 +1,11 @@
+require 'colorize'
+
+puts "-" * 20
+puts "Ruby version: #{RUBY_VERSION}"
+puts "Ruby platform: #{RUBY_PLATFORM}"
+puts "-" * 20
+
+puts "Roses are red".red
+puts "Violets are blue".blue
+puts "I can use JRuby/Gradle".green
+puts "And now you can too!".yellow
