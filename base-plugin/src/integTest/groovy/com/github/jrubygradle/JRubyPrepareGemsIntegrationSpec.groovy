2d4bd8a9f9cb9497570fe53f6e15de33bcab8d84
Schalk Cronje <ysb33r@gmail.com>
2019-06-15 14:54:22 +0200

    Verify that Gradle 5x. dependency locking works with GEMs (#372)
    Added at hack.commit.push 2019 with @ixschelruiz.

--- base-plugin/src/integTest/groovy/com/github/jrubygradle/JRubyPrepareGemsIntegrationSpec.groovy
+++ base-plugin/src/integTest/groovy/com/github/jrubygradle/JRubyPrepareGemsIntegrationSpec.groovy
@@ -1,3 +1,26 @@
+/*
+ * Copyright (c) 2014-2019, R. Tyler Croy <rtyler@brokenco.de>,
+ *     Schalk Cronje <ysb33r@gmail.com>, Christian Meier, Lookout, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package com.github.jrubygradle
 
 import com.github.jrubygradle.testhelper.IntegrationSpecification
@@ -36,6 +59,7 @@ class JRubyPrepareGemsIntegrationSpec extends IntegrationSpecification {
         withPreamble """repositories.ruby.gems()
             jrubyPrepare.outputDir = '${pathAsUriStr(projectDir)}'.toURI()
         """
+
         withDependencies """
             gems "rubygems:sinatra:1.4.5"
             gems "rubygems:rack:[0,)"
@@ -49,6 +73,48 @@ class JRubyPrepareGemsIntegrationSpec extends IntegrationSpecification {
         // since we need a version range in the setup the
         // resolved version here can vary over time
         new File(projectDir, "gems/rack-1.6.11").exists()
+    }
+
+    @IgnoreIf({ IntegrationSpecification.OFFLINE })
+    void "Check that GEM dependencies are locked"() {
+        setup:
+        File lockFile = new File(projectDir, 'gradle/dependency-locks/gems.lockfile')
+        withPreamble """repositories.ruby.gems()
+            jrubyPrepare.outputDir = '${pathAsUriStr(projectDir)}'.toURI()
+
+            dependencyLocking {
+                lockAllConfigurations()
+            }
+
+
+        """
+        withDependencies """
+            gems "rubygems:sinatra:1.4.5"
+            gems "rubygems:rack:[0,)"
+            gems "rubygems:lookout-rack-utils:5.0.0.49"
+        """
+
+        lockFile.parentFile.mkdirs()
+        lockFile.text = '''
+rubygems:concurrent-ruby:1.1.5
+rubygems:configatron:4.5.1
+rubygems:i18n:1.6.0
+rubygems:log4r:1.1.10
+rubygems:lookout-rack-utils:5.0.0.49
+rubygems:lookout-statsd:3.2.0
+rubygems:rack-graphite:1.6.0
+rubygems:rack-protection:1.5.5
+rubygems:rack:1.6.10
+rubygems:sinatra:1.4.5
+rubygems:tilt:2.0.9
+'''
+        when:
+        build()
+
+        then:
+        // since we need a version range in the setup the
+        // resolved version here can vary over time
+        new File(projectDir, "gems/rack-1.6.10").exists()
     }
 
     @IgnoreIf({ IntegrationSpecification.OFFLINE })
